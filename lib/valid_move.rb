3e3e5bcb72e1d297e2cfe6deaefff56cd5ac4ba9
Samuel Cornet <samuel@noukod.com>
2018-08-31 02:00:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index)
  !(board[index].nil? || board[index] == " ")
-    
+
 
   if index.between?(0,8) && !position_taken?(board, index)
       puts 'this is a valid move'
